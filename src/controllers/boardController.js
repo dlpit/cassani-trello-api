fe4e593cae322a0d41bb552c6966f530da7779c8
Huy Sad Nhan <cv.nhuytyl2603@gmail.com>
2024-05-20 15:28:20 +0700

    complete controller handling

---
+++ src/controllers/boardController.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-console */
+import { StatusCodes } from 'http-status-codes'
+
+const createNew = async (req, res, next) => {
+  try {
+    console.log(req.body)
+    res.status(StatusCodes.CREATED).json({ message : 'POST from Controller' })
+  } catch (error) {
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      errors: error.message
+    })
+  }
+}
+
+export const boardController = {
+  createNew
+}
