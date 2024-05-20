6ec3bf6d3694f40ac90d7d5c36b7639263271293
Huy Sad Nhan <cv.nhuytyl2603@gmail.com>
2024-05-20 11:49:52 +0700

    complete validation with joi

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { StatusCodes } from 'http-status-codes'
-import { boardRoutes } from '~/routes/v1/boardRoutes'
+import { boardRoute } from '~/routes/v1/boardRoute'
 const Router = express.Router()
 
 // Hello World API
@@ -9,6 +9,6 @@ Router.get('/status', (req, res) => {
 })
 
 // Board API
-Router.use('/board', boardRoutes)
+Router.use('/board', boardRoute)
 
 export const APIs_V1 = Router
---
+++ src/validations/boardValidation.js
@@ -0,0 +1,45 @@
+import Joi from 'joi'
+import { StatusCodes } from 'http-status-codes'
+
+const createNew = async (req, res, next) => {
+  const schema = Joi.object({
+    title: Joi.string().required().min(3).max(72).trim().strict().messages({
+      'string.base': 'Title must be a string',
+      'string.empty': 'Title is required',
+      'string.min': 'Title must have at least 3 characters',
+      'string.max': 'Title must have at most 72 characters',
+      'any.required': 'Title is required'
+    }),
+    description: Joi.string().required().min(3).max(255).trim().strict().messages({
+      'string.base': 'Description must be a string',
+      'string.empty': 'Description is required',
+      'string.min': 'Description must have at least 3 characters',
+      'string.max': 'Description must have at most 255 characters',
+      'any.required': 'Description is required'
+    }),
+  })
+  /**
+   * Note: Mặc định không cần phải custom message ở BE vì FE sẽ tự validate và custom message cho đẹp
+   * BE chỉ cần valide để đảm bảo dữ liệu đầu vào hợp lệ và trả về message của thư viện là được
+   * Quan trọng là việc Validate dữ liệu bắt buộc phải có từ 2 phía
+   */
+  try {
+    console.log(req.body)
+
+    // Chỉ định abortEarly: fasle để trả về tất cả các lỗi không chỉ lỗi đầu tiên
+    await schema .validateAsync(req.body, { abortEarly: false })
+
+    // next()
+    res.status(StatusCodes.CREATED).json({ message : 'POST from Validation' })
+  } catch (error) {
+    console.log(error)
+    console.log(new Error(error))
+    res.status(StatusCodes.UNPROCESSABLE_ENTITY).json({
+      errors: new Error(error).message
+    })
+  }
+}
+
+export const boardValidation = {
+  createNew
+}
