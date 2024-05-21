06f29ce7ac6682c0abdd0f3b1b242196d382b618
Huy Sad Nhan <cv.nhuytyl2603@gmail.com>
2024-05-21 16:31:22 +0700

    complete error handling

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -1,15 +1,20 @@
 /* eslint-disable no-console */
 import { StatusCodes } from 'http-status-codes'
+import ApiError from '~/utils/ApiError'
 
 const createNew = async (req, res, next) => {
   try {
-    console.log(req.body)
+    // console.log(req.body)
+    // console.log(req.query)
+    // console.log(req.params)
+    // console.log(req.files)
+    // console.log(req.cookies)
+    // console.log(req.jwtDecoded)
     res.status(StatusCodes.CREATED).json({ message : 'POST from Controller' })
-  } catch (error) {
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-      errors: error.message
-    })
-  }
+
+    // throw new ApiError(StatusCodes.BAD_GATEWAY, 'Error from Controller')
+
+  } catch (error) { next(error) }
 }
 
 export const boardController = {
