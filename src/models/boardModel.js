f10a3717ea7c0eed69efc8ca966632d1100d45ec
Huy Sad Nhan <cv.nhuytyl2603@gmail.com>
2024-05-23 10:29:32 +0700

    complete model layer

---
+++ src/models/boardModel.js
@@ -0,0 +1,29 @@
+import Joi from 'joi'
+import { OBJECT_ID_RULE, OBJECT_ID_RULE_MESSAGE } from '~/utils/validators'
+import { GET_DB } from '~/config/mongodb'
+// Define Collection (Name & Schema)
+const BOARD_COLLECTION_NAME = 'boards'
+const BOADRD_COLLECTION_SCHEMA = Joi.object({
+  title: Joi.string().required().min(3).max(72).trim().strict(),
+  slug: Joi.string().required().min(3).trim().strict(),
+  description: Joi.string().required().min(3).max(255).trim().strict(),
+  columnOrderIds: Joi.array().items(Joi.string().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE)).default([]),
+  createAt: Joi.date().timestamp('javascript').default(Date.now),
+  updateAt: Joi.date().timestamp('javascript').default(null),
+  _destroy: Joi.boolean().default(false)
+})
+
+const createNew = async (data) => {
+  try {
+    const createBoard = await GET_DB().collection(BOARD_COLLECTION_NAME).insertOne(data)
+    return createBoard
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
+export const boardModel = {
+  BOARD_COLLECTION_NAME,
+  BOADRD_COLLECTION_SCHEMA,
+  createNew
+}
