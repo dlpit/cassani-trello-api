36e7313ba479d27b1c890e8664717fdd53114e3e
Huy Sad Nhan <cv.nhuytyl2603@gmail.com>
2024-05-29 04:32:29 +0700

    update CORS not allow localhost

--- src/config/cors.js
+++ src/config/cors.js
@@ -6,11 +6,12 @@ import ApiError from '~/utils/ApiError'
 // Cấu hình CORS Option
 export const corsOptions = {
   origin: function (origin, callback) {
-    // Cho phép việc gọi API bằng POSTMAN trên môi trường dev,
-    // Thông thường khi sử dụng postman thì cái origin sẽ có giá trị là undefined
-    if (!origin && ENV.BUILD_MODE === 'dev') {
+    // Nêu là môi trường dev thì cho phép tất cả các domain
+    if (ENV.BUILD_MODE === 'dev') {
       return callback(null, true)
     }
+    // Ngươc lại nếu là môi trường production thì còn 1 trường hợp nữa cần xử lý
+    // ENV.BUILD_MODE === 'production'
 
     // Kiểm tra xem origin có phải là domain được chấp nhận hay không
     if (WHITELIST_DOMAINS.includes(origin)) {
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 // Những domain mà bạn muốn chấp nhận lấy tài nguyên từ server của bạn
 export const WHITELIST_DOMAINS = [
-  'http://localhost:5173'
+  // 'http://localhost:5173' // Không cần localhost vì ở file config/cors đã luôn chấp nhận môi trường
   // và các domain khác mà bạn muốn chấp nhận
 ]
 
