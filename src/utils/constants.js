773ba4a90d62377440e5a8ba58eadedeebac1ed7
Huy Sad Nhan <cv.nhuytyl2603@gmail.com>
2024-05-29 22:02:38 +0700

    Update add whitlist domains

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,6 +2,7 @@
 export const WHITELIST_DOMAINS = [
   // 'http://localhost:5173' // Không cần localhost vì ở file config/cors đã luôn chấp nhận môi trường
   // và các domain khác mà bạn muốn chấp nhận
+  'https://cassani.vercel.app/'
 ]
 
 export const BOARD_TYPES = {
